008c96948ae2af11e8750acb0b5ec5fd2e7adaf6
sm64isdabest <thiagopinheiro301007@gmail.com>
2025-05-21 12:03:00 -0300

    feat: Responsividade + JavaScript

---
+++ src/js/cadastroUser/mobilePop-up.js
@@ -0,0 +1,25 @@
+console.log("mobilePop-up.js loaded");
+
+const menuIcon = document.getElementById("list");
+const popupMenu = document.getElementById("popup-menu");
+
+menuIcon.addEventListener("click", function (e) {
+  e.stopPropagation();
+  if (popupMenu.style.display === "block") {
+    // menuIcon.style.backgroundColor = "white";
+    popupMenu.style.display = "none";
+    return;
+  }
+  // menuIcon.style.backgroundColor = "color: #593d36";
+  popupMenu.style.display = "block";
+});
+
+document.addEventListener("click", function (e) {
+  if (
+    popupMenu.style.display === "block" &&
+    !popupMenu.contains(e.target) &&
+    e.target !== menuIcon
+  ) {
+    popupMenu.style.display = "none";
+  }
+});
