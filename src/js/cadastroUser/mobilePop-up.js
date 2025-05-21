52ff9602faf65f6db8c87107af6bfe8ab2b79572
sm64isdabest <thiagopinheiro301007@gmail.com>
2025-05-21 17:15:31 -0300

    feat: Pop-up mobile finalizado

--- src/js/cadastroUser/mobilePop-up.js
+++ src/js/cadastroUser/mobilePop-up.js
@@ -6,11 +6,13 @@ const popupMenu = document.getElementById("popup-menu");
 menuIcon.addEventListener("click", function (e) {
   e.stopPropagation();
   if (popupMenu.style.display === "block") {
-    // menuIcon.style.backgroundColor = "white";
+    menuIcon.style.backgroundColor = "white";
+    menuIcon.style.color = "black"
     popupMenu.style.display = "none";
     return;
   }
-  // menuIcon.style.backgroundColor = "color: #593d36";
+  menuIcon.style.backgroundColor = "#593d36";
+  menuIcon.style.color = "white"
   popupMenu.style.display = "block";
 });
 
