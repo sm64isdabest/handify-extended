84a6d401fc02e6874097540189ebff476752fa56
ReynanSantts <reynanmesquita016@gmail.com>
2025-05-27 11:43:03 -0300

    feat: progresso na pagina de pesquisa

---
+++ src/js/database.js
@@ -0,0 +1,48 @@
+const produtos = [
+    {
+        nome: "Bolsa de Palha Natural Quadrada Artesanal Modelo Grande",
+        img: "../images/produtos/bolsa-flor.png",
+        preco: 280.15,
+        precoOriginal: 294.9,
+        desconto: 0.05, // 5%
+    },
+    {
+        nome: "Bolsa de Palha",
+        img: "../images/produtos/bolsa-flor.png",
+        preco: 279.9,
+        precoOriginal: 139.95,
+        desconto: 0.5, // 50%
+    },
+    {
+        nome: "Bolsa De Rattan Redonda Couro Artesanal Praia Mar Verão Vime",
+        img: "../images/produtos/bolsa-flor.png",
+        preco: 199.9,
+        precoOriginal: 199.9,
+        desconto: 0, // Sem desconto
+    },
+    {
+        nome: "Bolsa Transversal Loba Urbana Bag Caracol Terracota",
+        img: "../images/produtos/bolsa-flor.png",
+        preco: 240,
+        precoOriginal: 240,
+        desconto: 0, // Sem desconto
+    },
+    {
+        nome: "Bolsa Artesanal Madeira Envernizada Colorida Alça Carteira",
+        img: "../images/produtos/bolsa-flor.png",
+        preco: 67.49,   
+        precoOriginal: 74.99,
+        desconto: 0, // Sem desconto
+    }
+];
+
+const categorias = [
+    {
+        nome: "Roupas",
+        produtos: ["Camiseta", "Calça Jeans"]
+    },
+    {
+        nome: "Calçados",
+        produtos: ["Tênis"]
+    }
+];
