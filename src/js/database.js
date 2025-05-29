1f8e9b2bb3aa9f91341a9ca98c83807a6ecc09de
sm64isdabest <thiagopinheiro301007@gmail.com>
2025-05-29 11:45:52 -0300

    feat: progresso no database e no sistema de pesquisa

--- src/js/database.js
+++ src/js/database.js
@@ -1,48 +1,69 @@
-const produtos = [
+export const products = [
     {
-        nome: "Bolsa de Palha Natural Quadrada Artesanal Modelo Grande",
+        name: "Bolsa de Palha Natural Quadrada Artesanal Modelo Grande",
         img: "../images/produtos/bolsa-flor.png",
-        preco: 280.15,
-        precoOriginal: 294.9,
-        desconto: 0.05, // 5%
+        price: "R$280,15",
+        originalPrice: "R$294,90",
+        discount: 5, // 5%
     },
     {
-        nome: "Bolsa de Palha",
-        img: "../images/produtos/bolsa-flor.png",
-        preco: 279.9,
-        precoOriginal: 139.95,
-        desconto: 0.5, // 50%
+        name: "Bolsa de Palha",
+        img: "../images/produtos/bolsa-palha.png",
+        price: "R$279,90",
+        originalPrice: "R$139,95",
+        discount: 50, // 50%
     },
     {
-        nome: "Bolsa De Rattan Redonda Couro Artesanal Praia Mar Verão Vime",
-        img: "../images/produtos/bolsa-flor.png",
-        preco: 199.9,
-        precoOriginal: 199.9,
-        desconto: 0, // Sem desconto
+        name: "Bolsa de Rattan Redonda Couro Artesanal Praia Mar Verão Vime",
+        img: "../images/produtos/bolsa-redonda.png",
+        price: "R$123,90",
+        originalPrice: "R$123,90",
+        discount: 0, // Sem discount
     },
     {
-        nome: "Bolsa Transversal Loba Urbana Bag Caracol Terracota",
-        img: "../images/produtos/bolsa-flor.png",
-        preco: 240,
-        precoOriginal: 240,
-        desconto: 0, // Sem desconto
+        name: "Bolsa Transversal Loba Urbana Bag Caracol Terracota",
+        img: "../images/produtos/bolsa-vermelha.png",
+        price: "R$240,00",
+        originalPrice: "R$240,00",
+        discount: 0, // Sem discount
     },
     {
-        nome: "Bolsa Artesanal Madeira Envernizada Colorida Alça Carteira",
-        img: "../images/produtos/bolsa-flor.png",
-        preco: 67.49,   
-        precoOriginal: 74.99,
-        desconto: 0, // Sem desconto
+        name: "Bolsa Artesanal Madeira Envernizada Colorida Alça Carteira",
+        img: "../images/produtos/bolsa-colorida.png",
+        price: "R$67,49",
+        originalPrice: "R$74,99",
+        discount: 10, // 10%
+    },
+    {
+        name: "Bolsa Praia Feminina Tiracolo Estilo Clutch Alça Promocional Cor Palha - Bege Escuro",
+        img: "../images/produtos/bolsa-praia.png",
+        price: "R$56,35",
+        originalPrice: "R$59,95",
+        discount: 50, // 50%
+    },
+    {
+        name: "Bolsa de Praia Feminina Grande - Modelo Artesanal- Luxo",
+        img: "../images/produtos/bolsa-praia-clara.png",
+        price: "R$118,15",
+        originalPrice: "R$139,00",
+        discount: 15, // 15%
+    },
+    {
+        name: "Bolsa de Pérola Pequena",
+        img: "../images/produtos/bolsa-clara.png",
+        price: "R$69,5",
+        originalPrice: "R$139,00",
+        discount: 50, // 50%
     }
 ];
 
-const categorias = [
-    {
-        nome: "Roupas",
-        produtos: ["Camiseta", "Calça Jeans"]
-    },
-    {
-        nome: "Calçados",
-        produtos: ["Tênis"]
-    }
-];
+// const categorias = [
+//     {
+//         name: "Roupas",
+//         produtos: ["Camiseta", "Calça Jeans"]
+//     },
+//     {
+//         name: "Calçados",
+//         produtos: ["Tênis"]
+//     }
+// ];
