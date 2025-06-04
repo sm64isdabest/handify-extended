4d1e9187b3d0aba3ba8c5bda6585f476697767b7
sm64isdabest <thiagopinheiro301007@gmail.com>
2025-06-04 12:03:15 -0300

    feat: update no database, avaliações e autocomplete

--- src/js/database.js
+++ src/js/database.js
@@ -5,6 +5,8 @@ export const products = [
         price: "R$280,15",
         originalPrice: "R$294,90",
         discount: 5, // 5%
+        nota: 4.5,
+        avaliacoes: 120,
     },
     {
         name: "Bolsa de Palha",
@@ -12,20 +14,26 @@ export const products = [
         price: "R$279,90",
         originalPrice: "R$139,95",
         discount: 50, // 50%
+        nota: 4.1,
+        avaliacoes: 97,
     },
     {
         name: "Bolsa de Rattan Redonda Couro Artesanal Praia Mar Verão Vime",
         img: "../images/produtos/bolsa-redonda.png",
         price: "R$123,90",
         originalPrice: "R$123,90",
-        discount: 0, // Sem discount
+        discount: 0, // Sem desconto
+        nota: 4.2,
+        avaliacoes: 103,
     },
     {
         name: "Bolsa Transversal Loba Urbana Bag Caracol Terracota",
         img: "../images/produtos/bolsa-vermelha.png",
         price: "R$240,00",
         originalPrice: "R$240,00",
-        discount: 0, // Sem discount
+        discount: 0, // Sem desconto
+        nota: 4.9,
+        avaliacoes: 43,
     },
     {
         name: "Bolsa Artesanal Madeira Envernizada Colorida Alça Carteira",
@@ -33,6 +41,8 @@ export const products = [
         price: "R$67,49",
         originalPrice: "R$74,99",
         discount: 10, // 10%
+        nota: 4.3,
+        avaliacoes: 12,
     },
     {
         name: "Bolsa Praia Feminina Tiracolo Estilo Clutch Alça Promocional Cor Palha - Bege Escuro",
@@ -40,6 +50,8 @@ export const products = [
         price: "R$56,35",
         originalPrice: "R$59,95",
         discount: 50, // 50%
+        nota: 4.8,
+        avaliacoes: 35,
     },
     {
         name: "Bolsa de Praia Feminina Grande - Modelo Artesanal- Luxo",
@@ -47,6 +59,8 @@ export const products = [
         price: "R$118,15",
         originalPrice: "R$139,00",
         discount: 15, // 15%
+        nota: 4.6,
+        avaliacoes: 88,
     },
     {
         name: "Bolsa de Pérola Pequena",
@@ -54,6 +68,8 @@ export const products = [
         price: "R$69,50",
         originalPrice: "R$139,00",
         discount: 50, // 50%
+        nota: 4.7,
+        avaliacoes: 29,
     }
 ];
 
