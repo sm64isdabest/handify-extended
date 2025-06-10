42add6592a454147c40741375c149b6e0d44e89c
chaves777 <lc102434@gmail.com>
2025-06-10 11:56:35 -0300

    build:Adição de pop up (incompleto)

---
+++ src/js/paginaPrincipal/database.js
@@ -0,0 +1,82 @@
+export const products = [
+    {
+        code: 1001,
+        trackingCode: "TRK1001",
+        name: "Bolsa de Palha Natural Quadrada Artesanal Modelo Grande",
+        img: "../images/produtos/bolsa-flor.png",
+        description: "Bolsa de palha natural quadrada, artesanal, modelo grande, ideal para looks casuais e praianos, leve e versátil.",
+        price: "R$280,15",
+        originalPrice: "R$294,90",
+        discount: 5, // 5%
+    },
+    {
+        code: 1002,
+        trackingCode: "TRK1002",
+        name: "Bolsa de Palha",
+        img: "../images/produtos/bolsa-palha.png",
+        description: "Bolsa de palha com design artesanal, prática e elegante para diversas ocasiões.",
+        price: "R$279,90",
+        originalPrice: "R$139,95",
+        discount: 50, // 50%
+    },
+    {
+        code: 1003,
+        trackingCode: "TRK1003",
+        name: "Bolsa de Rattan Redonda Couro Artesanal Praia Mar Verão Vime",
+        img: "../images/produtos/bolsa-redonda.png",
+        description: "Bolsa de rattan redonda com couro artesanal, perfeita para praia, mar e verão.",
+        price: "R$123,90",
+        originalPrice: "R$123,90",
+        discount: 0, // Sem discount
+    },
+    {
+        code: 1004,
+        trackingCode: "TRK1004",
+        name: "Bolsa Transversal Loba Urbana Bag Caracol Terracota",
+        img: "../images/produtos/bolsa-vermelha.png",
+        description: "Bolsa transversal urbana com design caracol em tom terracota, estilo moderno.",
+        price: "R$240,00",
+        originalPrice: "R$240,00",
+        discount: 0, // Sem discount
+    },
+    {
+        code: 1005,
+        trackingCode: "TRK1005",
+        name: "Bolsa Artesanal Madeira Envernizada Colorida Alça Carteira",
+        img: "../images/produtos/bolsa-colorida.png",
+        description: "Bolsa artesanal de madeira envernizada, colorida, com alça estilo carteira.",
+        price: "R$67,49",
+        originalPrice: "R$74,99",
+        discount: 10, // 10%
+    },
+    {
+        code: 1006,
+        trackingCode: "TRK1006",
+        name: "Bolsa Praia Feminina Tiracolo Estilo Clutch Alça Promocional Cor Palha - Bege Escuro",
+        img: "../images/produtos/bolsa-praia.png",
+        description: "Bolsa de praia feminina tiracolo estilo clutch, cor palha bege escuro, promocional.",
+        price: "R$56,35",
+        originalPrice: "R$59,95",
+        discount: 50, // 50%
+    },
+    {
+        code: 1007,
+        trackingCode: "TRK1007",
+        name: "Bolsa de Praia Feminina Grande - Modelo Artesanal- Luxo",
+        img: "../images/produtos/bolsa-praia-clara.png",
+        description: "Bolsa de praia feminina grande, modelo artesanal de luxo, ideal para verão.",
+        price: "R$118,15",
+        originalPrice: "R$139,00",
+        discount: 15, // 15%
+    },
+    {
+        code: 1008,
+        trackingCode: "TRK1008",
+        name: "Bolsa de Pérola Pequena",
+        img: "../images/produtos/bolsa-clara.png",
+        description: "Bolsa pequena decorada com pérolas, elegante e sofisticada.",
+        price: "R$69,50",
+        originalPrice: "R$139,00",
+        discount: 50, // 50%
+    }
+];
