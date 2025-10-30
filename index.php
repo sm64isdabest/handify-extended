556bfeecc711c7904add003b1713938da9806074
Pedro-Henrique443 <dosanjospedro66@gmail.com>
2025-10-30 08:05:45 -0300

    refactor: correção de links da página principal

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <title>Handify - Página Principal</title>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="images/outros/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="css/index.css" />
   <link rel="stylesheet" href="css/track-pop-up.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
